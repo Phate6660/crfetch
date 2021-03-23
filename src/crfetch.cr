f5c1e4bdc25fce1948de260d0c369c3e3e9910fd
ValleyKnight <ValleyKnight@protonmail.com>
2021-03-22 22:16:34 -0400

    trim newlines from kernel/hostname

--- src/crfetch.cr
+++ src/crfetch.cr
@@ -1,9 +1,9 @@
 distro   = File.read_lines("/etc/os-release")[2].split(/=/)[1].lstrip('"').rstrip('"')
-hostname = File.read("/etc/hostname")
-kernel   = File.read("/proc/sys/kernel/osrelease")
+hostname = File.read("/etc/hostname").rstrip('\n')
+kernel   = File.read("/proc/sys/kernel/osrelease").rstrip('\n')
 
 message = "Distro   = #{distro}\n" \
-          "Hostname = #{hostname}" \
+          "Hostname = #{hostname}\n" \
           "Kernel   = #{kernel}"
 
 puts "#{message}"
